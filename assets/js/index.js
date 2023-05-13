86b1a35bbeb4db06bc132a78d2c1c96a76a6ee7e
grismendez <griskv2@gmail.com>
2023-05-13 17:33:58 -0600

    funcion depositos

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+//funcion para tranferir de cuenta 1 a 2
 function formula(){
     var cantidadT= parseInt(document.getElementById('cant').value);
     var cantidad1= parseInt(document.getElementById('c1').value);
@@ -5,7 +6,7 @@ function formula(){
     document.getElementById('c1').value=(cantidad1-cantidadT);
     document.getElementById('c2').value=(cantidad2+cantidadT);
 }
-
+//funcion para transferir de cuenta 2 a 1
 function formula2(){
     var cantidadT= parseInt(document.getElementById('cant').value);
     var cantidad1= parseInt(document.getElementById('c1').value);
@@ -13,3 +14,28 @@ function formula2(){
     document.getElementById('c1').value=(cantidad1+cantidadT);
     document.getElementById('c2').value=(cantidad2-cantidadT);
 }
+
+//funcion para hacer los depositos
+function formula3(){
+    var cantidadT= parseInt(document.getElementById('cant').value);
+    var cantidad1= parseInt(document.getElementById('c1').value);
+    var cantidad2= parseInt(document.getElementById('c2').value);
+    document.getElementById('c1').value=(cantidad1+cantidadT);
+    
+}
+
+
+//funcion para depositar a la cuenta 2 
+function formula4() {
+    var cantidadT= parseInt(document.getElementById('cant').value);
+    var cantidad1= parseInt(document.getElementById('c1').value);
+    var cantidad2= parseInt(document.getElementById('c2').value);
+    document.getElementById('c2').value=(cantidad2+cantidadT);
+}
+//funcion para hacer los retiros
+function formula5(){
+    var cantidadT= parseInt(document.getElementById('cant').value);
+    var cantidad1= parseInt(document.getElementById('c1').value);
+    var cantidad2= parseInt(document.getElementById('c2').value);
+    document.getElementById('c1').value=(cantidad1-cantidadT);
+} 
