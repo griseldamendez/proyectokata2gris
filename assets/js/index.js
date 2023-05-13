b0a16c0a9b4f0a94144e57c380e6b07209a8e8bf
grismendez <griskv2@gmail.com>
2023-05-13 17:39:10 -0600

    funcion retiros

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,10 +32,19 @@ function formula4() {
     var cantidad2= parseInt(document.getElementById('c2').value);
     document.getElementById('c2').value=(cantidad2+cantidadT);
 }
-//funcion para hacer los retiros
+//funcion para hacer retiro cuenta 1
 function formula5(){
     var cantidadT= parseInt(document.getElementById('cant').value);
     var cantidad1= parseInt(document.getElementById('c1').value);
     var cantidad2= parseInt(document.getElementById('c2').value);
     document.getElementById('c1').value=(cantidad1-cantidadT);
 } 
+
+//funcion para hacer retiro cuenta 2
+function formula6(){
+    var cantidadT= parseInt(document.getElementById('cant').value);
+    var cantidad1= parseInt(document.getElementById('c1').value);
+    var cantidad2= parseInt(document.getElementById('c2').value);
+    document.getElementById('c2').value=(cantidad2-cantidadT);
+} 
+
